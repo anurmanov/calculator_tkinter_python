2381ab5a0ba5392486b29cc84ab2b3f84d5c17a2
Alisher <38488987+anurmanov@users.noreply.github.com>
2021-03-09 19:40:17 +0300

    Update calc.py3.py
    refactoring

--- calc.py3.py
+++ calc.py3.py
@@ -1,12 +1,32 @@
+"""
+Tkinter calculator for big numbers
+"""
+
 import math
 import functools
 import tkinter as tk
 from tkinter import messagebox
 from tkinter.scrolledtext import ScrolledText
 
+
+#Tkinter global binded variables.
+#They are used for fetching the result, the arguments and operation type.
+e_result = None
+operation = None
+e_arg1 = None
+e_arg2 = None
+oper_names = (('+', '+'),
+              ('-', '-'),
+              ('*', '*'),
+              ('/', '/'),
+              ('^', '^'),
+              ('!  (factorial of the 1st arg)', '!'))
+
 def slice_n_calc_factorial(nums, d=1000):
-    """Recursive function slices list of numbers on sublists (d items in each one),
-    multiply items of sublists and return final factorial"""
+    """
+    Recursive function slices list of numbers on sublists (d items in each one),
+    multiply items of sublists and return final factorial
+    """
     if len(nums) > d:
         i = 1
         j = 1
@@ -24,45 +44,70 @@ def my_factorial(n):
     nums = (list(range(n)) + [n])[1:]
     return slice_n_calc_factorial(nums)
 
+def read_entry_argument(tkinter_entry):
+    try:
+        value = int(tkinter_entry.get())
+    except:
+        e_result.delete('1.0', tk.END)
+        e_result.insert(tk.INSERT, 'Some arguments have bad format!')
+        value = None
+    return value
+        
 def calc():
+    """
+    Main calculating function.
+    Uses tkinter binded variables fetching for arguments, operation and result
+    """
     e_result.delete('1.0', tk.END)
     e_result.insert(tk.INSERT, 'Calculating...')
     oper = str(operation.get())
-    d1 = int(e1.get())
+    d1 = read_entry_argument(e_arg1)
+    if not d1:
+        return
+    #factorial operation ! uses just 1st argument
     if oper != '!':
-        d2 = int(e2.get())
+        d2 = read_entry_argument(e_arg2)
+        if not d2:
+            return
+
     if oper == '+':
-        text = str(d1 + d2)
+        result = str(d1 + d2)
     elif oper == '-':
-        text = str(d1 - d2)
+        result = str(d1 - d2)
     elif oper == '*':
-        text = str(d1 * d2)
+        result = str(d1 * d2)
     elif oper == '/':
-        text = str(d1 / d2)
+        result = str(d1 / d2)
     elif oper == '^':
-        res = 1
-        d1
-        for i in range(d2):
-            res*=d1
-        text = str(res)
+        result = pow(d1, d2)
     elif oper == '!':
         text = str(my_factorial(d1))
-        #text = str(math.factorial(d1))
+        
     e_result.delete('1.0', tk.END)
     #result saved to file result.txt
     res_file = open('result.txt', 'w')
-    res_file.write(text)
+    res_file.write(result)
     res_file.close()
-    #if length of the result is more than 1000 symbols, then result number is not displayed
+    #if length of the result is more than 1000 symbols
+    #then result number is not displayed
     if len(text) > 1000:
         e_result.insert(tk.INSERT, 'Result saved to result.txt')
-        messagebox.showinfo(title='Information', message='Result saved to result.txt')
+        messagebox.showinfo(title='Information',
+                            message='Result saved to result.txt')
     else:
         e_result.insert(tk.INSERT, text)
 
-oper_names = [('+', '+'),  ('-', '-'), ('*', '*'), ('/', '/'), ('^', '^'), ('!  (factorial of the 1st arg)', '!')]
 
 def build_tkinter_interface():
+    """
+    Application form builder function
+    """
+    global e_result
+    global operation
+    global e_arg1
+    global e_arg2
+    
+    #TK subsystem initializing
     root = tk.Tk()
     root.title('Calculator')
     
@@ -73,8 +118,8 @@ def build_tkinter_interface():
     #label "1st argument"
     lbl1 = tk.Label(frame, text='1st argument :')
     lbl1.pack(side=tk.LEFT)
-    e1 = tk.Entry(frame, width=30)
-    e1.pack(side=tk.LEFT)
+    e_arg1 = tk.Entry(frame, width=30)
+    e_arg1.pack(side=tk.LEFT)
     #binded variable
     operation = tk.StringVar()
     
@@ -86,7 +131,11 @@ def build_tkinter_interface():
     frameForOps = tk.Frame(frame)
     frameForOps.pack(side=tk.LEFT)
     for oper_name, val in oper_names:
-        r = tk.Radiobutton(frameForOps, text=oper_name, padx = 20, variable=operation, value=val)
+        r = tk.Radiobutton(frameForOps,
+                           text=oper_name,
+                           padx = 20,
+                           variable=operation,
+                           value=val)
         r.pack(side=tk.LEFT)
         r.select()
 
@@ -95,8 +144,8 @@ def build_tkinter_interface():
     frame.pack(anchor=tk.W)
     lbl3 = tk.Label(frame, text='2nd argument :')
     lbl3.pack(side=tk.LEFT)
-    e2 = tk.Entry(frame, width=30)
-    e2.pack(side=tk.LEFT)
+    e_arg2 = tk.Entry(frame, width=30)
+    e_arg2.pack(side=tk.LEFT)
 
     #button "calc"
     b = tk.Button(text='   calc   ', command=calc)
@@ -113,4 +162,4 @@ def build_tkinter_interface():
     root.mainloop()
 
 if __name__ == '__main__':
-    build_tkinter_interface()    
+    build_tkinter_interface()
